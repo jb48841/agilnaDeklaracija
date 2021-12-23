5b24fc1b5de98bbddf6352db880c3977e2f366db
filipsaric1 <fico.zd@hotmail.com>
2021-12-23 11:48:19 +0100

    optimizirano scrapeanje

--- avp_backend/index.js
+++ avp_backend/index.js
@@ -1,9 +1,12 @@
-const app = require('./app')
+const app = require("./app");
+const scrape = require("./scrape");
+
+scrape();
 
 app.listen(8080, function (err) {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
-  console.log('Server radi na portu', 8080)
-})
+  console.log("Server radi na portu", 8080);
+});
